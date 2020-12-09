af61100524070c24e5fae00f17e0cb88026e0104
Pavan Katepalli <pavankatepalli@Pavans-MBP.hsd1.ca.comcast.net>
2020-12-09 11:49:32 -0800

    get messages for room and send to show view

--- lib/test_web/controllers/chat_controller.ex
+++ lib/test_web/controllers/chat_controller.ex
@@ -1,7 +1,11 @@
 defmodule TestWeb.ChatController do
   use TestWeb, :controller
+  # Let's also alias our module here to so we can call it without the prefix
+  alias Test.Chats
 
   def show(conn, %{"id" => room}) do
-    render(conn, "show.html", room: room)
+  	messages = Chats.list_messages_by_room(room)
+
+    render(conn, "show.html", room: room, messages: messages)
   end
 end
